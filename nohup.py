ec1b29ffaa5fdb68abd758119daf850bbedec4b6
bithuanglq <46642752+bithuanglq@users.noreply.github.com>
2022-11-10 14:23:50 +0800

    Update nohup.py

--- nohup.py
+++ nohup.py
@@ -5,4 +5,4 @@ import os
 if __name__=='__main__':
     if True:
         # 在 tmux 中运行该程序
-        os.system('nohup python -m torch.distributed.launch --nproc_per_node=4 /home/hlq/Project/BraTS/code/ResTransGAN_with_cnn_resnet.py > nohup_v2.out 2>&1 &')
+        os.system('nohup python -m torch.distributed.launch --nproc_per_node=4 your_python_file_path > nohup.out 2>&1 &')
